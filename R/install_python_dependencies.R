943e4dabfd48ce7afeda7bcade3b4762d240c786
Ethan <eb565@cornell.edu>
2022-03-28 09:39:30 -0400

    add dots for python dependencies installer
    add dots

--- R/install_python_dependencies.R
+++ R/install_python_dependencies.R
@@ -1,8 +1,9 @@
 #' @name install_python_dependencies
 #' @title Basic function to install python dependencies.
 #' @import reticulate
+#' @param ... Additional commands to \code{\link[reticulate]{py_install}}
 #' @export
-install_python_dependencies <- function(){
-  py_install(c("scipy","numpy", "pandas"))
+install_python_dependencies <- function(...){
+  py_install(c("scipy","numpy", "pandas"), ...)
   py_install("aston", pip = T)
 }
