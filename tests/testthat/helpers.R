2a3bb7d8dce085250ae3c2b7b414d26136fc5db0
Ethan <eb565@cornell.edu>
2022-04-07 14:58:10 -0400

    move helpers to helpers file

---
+++ tests/testthat/helpers.R
@@ -0,0 +1,12 @@
+
+# helper function to test equality
+elementwise.all.equal <- Vectorize(function(x, y, ...) {isTRUE(all.equal(x, y, ...))})
+
+# helper function to skip tests if we don't have the right python dependencies
+skip_if_missing_dependecies <- function() {
+  reqs <- c("scipy","numpy", "aston", "pandas")
+  have_reqs <- sapply(reqs, py_module_available)
+  if (mean(have_reqs) < 1)
+    skip(paste("required packages", reqs[!have_reqs],
+               "not available for testing"))
+}
