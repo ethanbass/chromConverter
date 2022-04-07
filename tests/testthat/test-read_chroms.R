0617c8f44e5f9c8807672c9f716cf3590f7dc742
Ethan <eb565@cornell.edu>
2022-04-07 14:49:05 -0400

    Update test-read_chroms.R
    add tests for read_chroms

--- tests/testthat/test-read_chroms.R
+++ tests/testthat/test-read_chroms.R
@@ -20,3 +20,15 @@ test_that("chemstation uv importer works", {
   x1 <- uv_converter("testdata/DAD1.uv")
   l <- length(which(!elementwise.all.equal(x[,2], x1[,"220.0"])))
   expect_equal(l,0)})
+
+test_that("read_chroms works", {
+  skip_if_missing_dependecies()
+  path<-"testdata/DAD1.CSV"
+  x <- read.csv(path, fileEncoding = "utf-16")
+  paths <- rep("testdata/DAD1.uv",2)
+  x1 <- read_chroms(paths, format.in = "chemstation.uv",find_files = FALSE)
+  l <- length(which(!elementwise.all.equal(x[,2], x1[[1]][,"220.0"])))
+  expect_equal(l,0)
+  expect_equal(length(x1), length(paths))
+  })
+
