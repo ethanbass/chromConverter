438da1ca9f16c80ae3b01f7345bdf069abf371da
Ethan Bass <ethanbass@gmail.com>
2022-07-04 10:47:08 -0400

    revert changes to pkgdown.yaml

--- R/zzz.R
+++ R/zzz.R
@@ -4,13 +4,6 @@
     use_miniconda("r-reticulate")
     reticulate::configure_environment("chromConverter")
   }
-  # if (!env){
-  # reqs <- c("pandas","scipy","numpy","aston")
-  # reqs_available <- sapply(reqs, py_module_available)
-  # if (!all(reqs_available)){
-  #   py_install(reqs[which(!reqs_available)], pip = TRUE)
-  # }
-  # }
   trace_file <<- reticulate::import("aston.tracefile", delay_load = TRUE)
   pd <<- reticulate::import("pandas", delay_load = TRUE)
   csv <<- reticulate::import("csv", delay_load = TRUE)
