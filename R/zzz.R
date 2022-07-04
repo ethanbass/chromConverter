bb15e5d8186547190f7c7c5f16341861ddf62f51
Ethan Bass <ethanbass@gmail.com>
2022-07-03 22:46:24 -0400

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,10 @@
 .onLoad <- function(libname, pkgname){
+  reticulate::configure_environment("chromConverter")
   reqs <- c("pandas","scipy","numpy","aston")
   reqs_available <- sapply(reqs, py_module_available)
   if (!all(reqs_available)){
     py_install(reqs[which(!reqs_available)], pip = TRUE)
   }
-  reticulate::configure_environment("chromConverter")
   trace_file <<- reticulate::import("aston.tracefile", delay_load = TRUE)
   pd <<- reticulate::import("pandas", delay_load = TRUE)
   csv <<- reticulate::import("csv", delay_load = TRUE)
