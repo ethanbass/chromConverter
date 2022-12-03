25235d1a489fc5bbdfa0e2863489875e755ab4be
Ethan Bass <ethanbass@gmail.com>
2022-12-02 20:13:34 -0500

    try to add error handling to .onLoad function

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,8 @@
 .onLoad <- function(libname, pkgname){
-  env <- reticulate::configure_environment("chromConverter")
-  trace_file <<- reticulate::import("aston.tracefile", delay_load = TRUE)
-  pd <<- reticulate::import("pandas", delay_load = TRUE)
-  csv <<- reticulate::import("csv", delay_load = TRUE)
-  rb_read <<- reticulate::import("rainbow.__init__", delay_load=TRUE)
-  rb_parse_agilent <<- reticulate::import("rainbow.agilent", delay_load=TRUE)
+  try(env <- reticulate::configure_environment("chromConverter"))
+  try(trace_file <<- reticulate::import("aston.tracefile", delay_load = TRUE))
+  try(pd <<- reticulate::import("pandas", delay_load = TRUE))
+  try(csv <<- reticulate::import("csv", delay_load = TRUE))
+  try(rb_read <<- reticulate::import("rainbow.__init__", delay_load=TRUE))
+  try(rb_parse_agilent <<- reticulate::import("rainbow.agilent", delay_load=TRUE))
 }
