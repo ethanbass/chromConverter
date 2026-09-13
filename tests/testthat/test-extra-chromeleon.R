6501c87a3f33031dd6ff3637165bcaa2a148e05b
Ethan Bass <ethanbass@gmail.com>
2026-09-13 13:19:21 -0400

    ci: add additional tests for asm, chromeleon, waters

--- tests/testthat/test-extra-chromeleon.R
+++ tests/testthat/test-extra-chromeleon.R
@@ -22,6 +22,24 @@ test_that("read_chroms can read 'Chromeleon' comma-separated files", {
   expect_equal(as.numeric(rownames(x)), x1[,1])
   expect_equal(x[,1], x1[,2], ignore_attr = TRUE)
   expect_equal(attr(x1, "data_format"), "long")
+
+  expect_equal(sum(x),
+    25631.528997)
+  expect_equal(range(x),
+    c(-0.7749, 210.061603))
+  expect_equal(which.max(x),
+    799L)
+  expect_equal(unname(head(x[, 1], 5)),
+    c(-0.0896, -0.03875, -0.0199, -0.0107, -0.0044))
+  expect_equal(unname(tail(x[, 1], 5)),
+    c(0.62745, 0.6253, 0.6306, 0.63075, 0.62815))
+  expect_equal(unname(x[round(seq(1, nrow(x), length.out = 15)), 1]),
+    c(-0.0896, -0.01275, 6.94385, 5.90415, 7.08265, 8.47905, 9.69805, 
+    10.91685, 11.9916, 12.9037, 13.895, 6.6609, 0.94275, 0.4297, 0.62815))
+  expect_equal(head(as.numeric(rownames(x)), 3),
+    c(0, 0.016667, 0.033333))
+  expect_equal(tail(as.numeric(rownames(x)), 3),
+    c(53.966667, 53.983333, 54)) 
 })
 
 test_that("read_chroms can read 'Chromeleon' period-separated files", {
@@ -43,6 +61,21 @@ test_that("read_chroms can read 'Chromeleon' period-separated files", {
   expect_equal(colnames(x1), c("rt", "intensity"))
   expect_equal(as.numeric(rownames(x)), x1[,1])
   expect_equal(x[,1], x1[,2], ignore_attr = TRUE)
+
+  expect_equal(sum(x),
+    -0.23485)
+  expect_equal(range(x),
+    c(-0.0896, -0.0044))
+  expect_equal(which.max(x),
+    5L)
+  expect_equal(unname(head(x[, 1], 5)),
+    c(-0.0896, -0.03875, -0.0199, -0.0107, -0.0044))
+  expect_equal(unname(tail(x[, 1], 5)),
+    c(-0.01355, -0.00705, -0.0116, -0.0142, -0.0251))
+  expect_equal(head(as.numeric(rownames(x)), 3),
+    c(0, 0.016667, 0.033333))
+  expect_equal(tail(as.numeric(rownames(x)), 3),
+    c(0.116667, 0.133333, 0.15)) 
 })
 
 test_that("read_chroms can read 'Chromeleon' 3D data files", {
@@ -72,4 +105,20 @@ test_that("read_chroms can read 'Chromeleon' 3D data files", {
   expect_equal(as.numeric(rownames(x)), x1[x1$lambda==200,1])
   expect_equal(nrow(x1), ncol(x)*nrow(x))
   expect_equal(attr(x1, "data_format"), "long")
+
+  expect_equal(sum(x),
+    -1759379.88521591)
+  expect_equal(range(x),
+    c(-395.993972, 221.548094))
+  expect_equal(unname(colSums(x)[c(1, 151, 301)]),
+    c(-788845.459228999, 1621.582252, -7921.156766))
+  expect_equal(unname(x[1, 1:5]),
+    c(-0.001371, -0.000417, 0, 0.000179, -0.000656))
+  expect_equal(unname(x[nrow(x), 297:301]),
+    c(0.035107, 0.011384, 0.010073, 0.046074, 0.044167))
+  expect_equal(unname(x[3000, c(1, 151, 301)]),
+    c(-200.909269, 0.459433, -2.27046))
+  expect_equal(unname(x[round(seq(1, nrow(x), length.out = 12)), 151]),
+    c(0.000238, 0.02569, -0.078201, -0.191271, 0.13274, 0.446737, 0.407219, 
+    0.350237, 0.499487, 0.180602, 0.363827, 0.016391)) 
 })
--- tests/testthat/test-extra-waters.R
+++ tests/testthat/test-extra-waters.R
@@ -19,6 +19,22 @@ test_that("read_chroms can read 'Waters ARW' PDA files", {
   expect_equal(colnames(x1), c("rt", "lambda", "intensity"))
   expect_s3_class(x1[1], "data.frame")
   expect_equal(attr(x1, "data_format"), "long")
+
+  expect_equal(sum(x[[1]]),
+    37775.669732916)
+  expect_equal(range(x[[1]]),
+    c(-0.0326082, 2.335802))
+  expect_equal(unname(colSums(x[[1]])[c(1, 245, 489)]),
+    c(1903.4079729, -6.49555660009999, -23.1140699741))
+  expect_equal(unname(x[[1]][1, 1:5]),
+    c(0, 0, 0, 0, 0))
+  expect_equal(unname(x[[1]][nrow(x[[1]]), 485:489]),
+    c(-0.0098628, -0.0098831, -0.0092422, -0.0093695, -0.0095033))
+  expect_equal(unname(x[[1]][3000, c(1, 245, 489)]),
+    c(0.2262086, -0.0014038, -0.0037171))
+  expect_equal(unname(x[[1]][round(seq(1, nrow(x[[1]]), length.out = 12)), 245]),
+    c(0, 0.0017719, -0.0014524, -0.0014026, -0.0010072, -0.0012787, 
+    -0.0015119, -0.0017052, -0.0018358, -0.0019263, -0.0020249, -0.0019739)) 
 })
 
 test_that("read_chroms can read 'Waters RAW' files", {
@@ -51,4 +67,57 @@ test_that("read_chroms can read 'Waters RAW' files", {
                     sparse = FALSE)[[1]]
   expect_equal(nrow(x2$MS), nrow(x$MS)*ncol(x$MS))
   expect_equal(colnames(x2$MS), c("rt", "mz", "intensity"))
+
+  expect_equal(sum(x1$CAD),
+    11141.2282361984)
+  expect_equal(range(x1$CAD),
+    c(11.0376472473145, 24.6099243164062))
+  expect_equal(which.max(x1$CAD),
+    80L)
+  expect_equal(unname(head(x1$CAD[, 1], 5)),
+    c(11.0376472473145, 11.1447868347168, 11.2513303756714, 11.3565330505371, 
+    11.4600954055786))
+  expect_equal(unname(tail(x1$CAD[, 1], 5)),
+    c(12.7757186889648, 12.7682685852051, 12.7696094512939, 12.7807855606079, 
+    12.803882598877))
+  expect_equal(unname(x1$CAD[round(seq(1, nrow(x1$CAD), length.out = 15)), 1]),
+    c(11.0376472473145, 14.2595767974854, 19.4326629638672, 20.321964263916, 
+    19.7731552124023, 16.5281295776367, 14.0557289123535, 12.8301086425781, 
+    12.2373399734497, 11.3967657089233, 11.3832054138184, 11.5787086486816, 
+    11.5755796432495, 11.7000045776367, 12.803882598877))
+  expect_equal(head(as.numeric(rownames(x1$CAD)), 3),
+    c(0, 0.00333333341404796, 0.00666666682809591))
+  expect_equal(tail(as.numeric(rownames(x1$CAD)), 3),
+    c(2.53999996185303, 2.54333329200745, 2.54666662216187)) 
+})
+
+test_that("read_waters_arw decodes 2D values exactly", {
+  skip_on_cran()
+  skip_if_not_installed("chromConverterExtraTests")
+
+  path <- system.file("waters.arw", package = "chromConverterExtraTests")
+  skip_if_not(file.exists(path))
+
+  x <- read_chroms(path, format_in = "waters_arw", progress_bar = FALSE)[[1]]
+
+  expect_equal(dim(x),
+    c(6601L, 1L))
+  expect_equal(sum(x),
+    144023.407497391)
+  expect_equal(range(x),
+    c(-0.7805328, 366.7791))
+  expect_equal(which.max(x),
+    3324L)
+  expect_equal(unname(head(x[, 1], 5)),
+    c(0, 0, 0, 0, 0))
+  expect_equal(unname(tail(x[, 1], 5)),
+    c(0.4577484, 0.4567719, 0.4557953, 0.4557953, 0.4577484))
+  expect_equal(unname(x[round(seq(1, nrow(x), length.out = 15)), 1]),
+    c(0, -0.197525, -0.4289703, -0.6008453, -0.7795563, -0.5647125, 18.26537, 
+    357.6794, 39.01439, 14.50072, 5.28978, 2.854233, 1.702866, 0.9733734, 
+    0.4577484))
+  expect_equal(head(as.numeric(rownames(x)), 3),
+    c(0, 0.008333333, 0.01666667))
+  expect_equal(tail(as.numeric(rownames(x)), 3),
+    c(54.98333, 54.99167, 55))
 })
